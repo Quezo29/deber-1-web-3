311eb4b2c788d9d4dc18ba6a267d9c6e894a87ac
Ricardo <rsql12345@gmail.com>
2026-08-26 12:44:14 -0500

    se creo el archivo juegos.js

---
+++ Programa/Version_JS/src/juegos.js
@@ -0,0 +1,14 @@
+export const criaturas = [
+  {id: 1, nombre: "Zelda: Ocarina of Time", genero: "accion", desarrollador: "Nintendo", plataforma: "Nintendo 64",calificacion: 99 },
+
+  {id: 1, nombre: "Portal 2",  genero: "Puzzle", desarrollador: "Valve", plataforma: "PC", calificacion: 95 },
+
+  {id: 1, nombre: "Bubsy 3D", genero: "Plataformas", desarrollador: "Eidetic", plataforma: "Play Station 1",  calificacion: 36 },
+
+  {id: 1, nombre: "Super Smash Bros", genero: "Lucha", desarrollador: "Nintendo", plataforma: "Nintendo 64",  calificacion: 79 },
+
+  {id: 1, nombre: "EL Chavo",  genero: "Fiesta",     desarrollador: "Kaxan Media Group", plataforma: "Wii",  calificacion: 63},
+
+  {id: 1, nombre: "Metal Gear Solid",  genero: "Sigilo",     desarrollador: "Konami", plataforma: "Play Station 1",  calificacion: 94 },
+
+];
